75e66572b6c99f57727f4fc0fcca619f397680ac
Sergey Milenko <ssmilenko@gmail.com>
2020-05-09 23:47:42 +0300

    some changes with tittle

--- client/components/main.js
+++ client/components/main.js
@@ -8,11 +8,9 @@ const Dummy = () => {
       <Head title="Hello" />
       <div className="flex items-center justify-center h-screen">
         <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
-          <div id="title">
-            Main
-            <Link to="/dashboard/profile/27f74de3-e577-439d-9daf-389da0ede196"> Go To Profile </Link>
-            <Link to="/dashboard"> Go To Root </Link>
-          </div>
+          <div id="title"> Main</div>
+          <Link to="/dashboard/profile/27f74de3-e577-439d-9daf-389da0ede196"> Go To Profile </Link>
+          <Link to="/dashboard"> Go To Root </Link>
         </div>
       </div>
     </div>
