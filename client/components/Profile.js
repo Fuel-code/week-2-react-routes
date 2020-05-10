a46bfa39ba28623f4f6ad3c0290f77aa3277da4f
Sergey Milenko <ssmilenko@gmail.com>
2020-05-10 22:10:11 +0300

    some changes

--- client/components/Profile.js
+++ client/components/Profile.js
@@ -1,20 +1,14 @@
 import React from 'react'
 import { Link, useParams } from 'react-router-dom'
-import Head from './head'
 
 const Dummy = () => {
   const { user } = useParams()
   return (
     <div>
-      <Head title="Hello" />
-      <div className="flex items-center justify-center h-screen">
-        <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
-          <Link to="/dashboard/main"> Go To Main </Link>
-          <Link to="/dashboard"> Go To Root </Link>
-          <div id="title">Profile</div>
-          <div id="username">{user}</div>
-        </div>
-      </div>
+      <Link to="/dashboard/main"> Go To Main </Link>
+      <Link to="/dashboard"> Go To Root </Link>
+      <div id="title">Profile</div>
+      <div id="username">{user}</div>
     </div>
   )
 }
